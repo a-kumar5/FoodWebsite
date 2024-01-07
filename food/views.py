2e2e6c0ee2e8321953269a2344415ff181ff2d7f
a-kumar5 <ayush.1306.kumar@gmail.com>
2024-01-07 13:39:41 +0530

    Updated food.views index function to retrieve data from database and display

--- food/views.py
+++ food/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime, timedelta
+from .models import Item
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the index")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 
 def item(request):
