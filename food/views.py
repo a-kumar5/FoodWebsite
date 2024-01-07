9d0ddf4bdd963a667e486455403c99914a21bb0e
a-kumar5 <ayush.1306.kumar@gmail.com>
2024-01-07 19:31:26 +0530

    Added a template and index function changes to display the data from database

--- food/views.py
+++ food/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime, timedelta
+
+from django.template import loader
+
 from .models import Item
 
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    context = {
+        'item_list': item_list,
+    }
+    return render(request,'food/index.html',context)
 
 
 def item(request):
