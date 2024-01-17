e15875fae44019defa6dbf3ef673fa095f5cae66
a-kumar5 <ayush.1306.kumar@gmail.com>
2024-01-18 01:33:40 +0530

    Added profile page, Changes to logout working.

--- MyFoodWebsite/settings.py
+++ MyFoodWebsite/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'food:index'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views.decorators.http import require_http_methods
 from .forms import RegisterForm
@@ -16,7 +17,13 @@ def register(request: HttpResponse) -> HttpResponse:
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Welcome {username}, your account is created')
-            return redirect('food:index')
+            return redirect('login')
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+
+@login_required()
+def profile_page(request: HttpResponse) -> HttpResponse:
+    return render(request, 'users/profile.html')
